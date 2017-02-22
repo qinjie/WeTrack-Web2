cbd20ccd716d1be5ca8c044785ef15762f2a0eca
HoaNP <hoanpse03457@fpt.edu.vn>
2017-02-22 16:12:57 +0800

    add filter for location history

--- backend/views/location-history/index.php
+++ backend/views/location-history/index.php
@@ -14,6 +14,22 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php
+        $data = [];
+        $users = \common\models\User::find()->all();
+        foreach ($users as $value => $item){
+//            echo $item->id . " -- " . $item->username . "\n";
+            switch ($item->role){
+                case 2: $data[$item->id] = "Raspberry " . $item->id;
+                    break;
+                case 5: $data[$item->id] = "Anonymous " . $item->id;
+                    break;
+                default:
+                    $data[$item->id] = $item->username;
+            }
+        }
+//        var_dump($users);
+    ?>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
@@ -24,6 +40,8 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             [
                 'attribute' => 'beacon_id',
+                'filter' => Html::activeDropDownList($searchModel, 'beacon_id', \yii\helpers\ArrayHelper::map(\common\models\Beacon::find()->asArray()->all(), 'id', 'id')
+                    ,['class'=>'form-control','prompt' => 'Select Status']),
                 'format' => 'raw',
                 'value' => function ($data) {
                     return Html::a($data->beacon_id, ['/beacon/view', 'id' => $data->beacon_id]);
@@ -33,6 +51,7 @@ $this->params['breadcrumbs'][] = $this->title;
 //            'locator_id',
             [
                 'attribute' => 'user_id',
+                'filter' => Html::activeDropDownList($searchModel, 'user_id',$data,['class'=>'form-control','prompt' => 'Select Detector Name']),
                 'label' => 'Detector Name',
                 'value' => function($data){
                     switch ($data->user->role) {
@@ -45,7 +64,10 @@ $this->params['breadcrumbs'][] = $this->title;
 
             [
                 'label' => 'Resident Name',
-                'attribute' => 'user_id',
+                'attribute' => 'beacon_id',
+//                'filter' => Html::activeDropDownList($searchModel, 'beacon_id',$data,['class'=>'form-control','prompt' => 'Select Detector Name']),
+//                'filter' => Html::activeDropDownList($searchModel, 'beacon.resident_id', \yii\helpers\ArrayHelper::map(\common\models\Resident::find()->asArray()->all(), 'id', 'fullname')
+//                    ,['class'=>'form-control','prompt' => 'Select Status']),
                 'format' => 'raw',
                 'value' => function ($data) {
                     return Html::a($data->beacon->resident->fullname, ['/resident/view', 'id' => $data->beacon->resident->id]);
