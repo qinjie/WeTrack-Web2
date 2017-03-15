c0b7dd712884b2f985440d7bbf3deea5759a4b9d
hoanghiepnb <hiepbh195@gmail.com>
2017-03-15 14:43:28 +0800

    edit

--- pyfcm/RPI/Floor/main.py
+++ pyfcm/RPI/Floor/main.py
@@ -31,8 +31,7 @@ if __name__ == '__main__':
         serial_number = getSerial.getserial()
         post_data = {'serial_number': serial_number}
         get_response = requests.post(url=url, data=post_data)
-        print
-        get_response.text
+
         listReceiveBeacon = []
         listReceiveBeaconID = []
         result = json.loads(get_response.text)
@@ -42,12 +41,10 @@ if __name__ == '__main__':
         dev_id = 0
         try:
             sock = bluez.hci_open_dev(dev_id)
-            print
-            "ble thread started"
+            print "ble thread started"
 
         except:
-            print
-            "error accessing bluetooth device..."
+            print "error accessing bluetooth device..."
             sys.exit(1)
 
         blescan.hci_le_set_scan_parameters(sock)
@@ -63,8 +60,7 @@ if __name__ == '__main__':
                 listReceiveBeacon = []
                 listReceiveBeaconID = []
                 listReceiveBeacon = blescan.parse_events(sock, 10)
-                print
-                "----------"
+                print ("----------")
 
                 headers = {'Authorization': '%s' % auth}
                 url = "http://128.199.93.67/WeTrack/api/web/index.php/v1/location-history/alive"
